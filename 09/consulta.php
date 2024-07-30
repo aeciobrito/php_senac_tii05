f65cb9a400e43b1fda47d89fee7a6800ce9dbf4e
aeciobrito <aeciobrito@gmail.com>
2024-07-29 21:44:43 -0300

    Consultas com boas praticas no PDO

--- 09/consulta.php
+++ 09/consulta.php
@@ -4,10 +4,17 @@ $user = 'usuario_lista_compras';
 $password = 'Senha@123';
 
 try {
-    // https://www.php.net/manual/en/pdo.connections.php
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conectado ao banco de dados com sucesso!";
+    
+    $sql = "SELECT * FROM lista_compras.itens_compra";
+    $stmt = $dbh->query($sql);
+
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo '<pre>';
+    print_r($result);
+    echo '</pre>';
 } catch(PDOException $e) {
     echo "Erro ao se conectar ao banco: <br>";
     echo $e;
