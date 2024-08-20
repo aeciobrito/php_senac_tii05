bb2be322fe693d984c6591eed06ec5bfd8b53e51
aeciobrito <aeciobrito@gmail.com>
2024-08-20 19:33:45 -0300

    Fix - Ausencia de update e retornos na API

--- ComprasAPI/api.php
+++ ComprasAPI/api.php
@@ -10,15 +10,16 @@ switch ($acao) {
         break;
     case 'add':
         $data = json_decode(file_get_contents('php://input'), true);
-        DatabaseRepository::addItem($data['nome_produto'], $data['quantidade']);
+        echo DatabaseRepository::addItem($data['nome_produto'], $data['quantidade']);
         break;
     case 'update':
         $id = $_GET['id'];
         $data = json_decode(file_get_contents('php://input'), true);
+        echo DatabaseRepository::updateItem($id, $data['nome_produto'], $data['quantidade'], $data['comprado']);
         break;
     case 'delte':
         $id = $_GET['id'];
-        DatabaseRepository::deleteItem($id); 
+        echo DatabaseRepository::deleteItem($id); 
         break;
     default:
         echo json_encode(['error' => 'Acao Invalida']);
